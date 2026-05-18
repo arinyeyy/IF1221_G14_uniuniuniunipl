068f2b0729cc2b0359c461a424036d3b038b9e36
arinyeyy <arinikarimatun.ak@gmail.com>
2026-05-19 06:18:45 +0700

    fix: update findAllTangan

--- src/findAll.pl
+++ src/findAll.pl
@@ -6,4 +6,8 @@ findPemain(Input, Input) :- \+ (pemain(X), \+ isMember(X,Input)).
 findPemain(Input, Output) :- pemain(X), \+ isMember(X,Input), !, findPemain([X|Input], Output).
 findAllPemain(List) :- findPemain([], List).
 
+findTangan(Nama, Input, Input) :- \+ (kartuPemain(Nama, kartu(W, J)), \+ isMember(kartu(W, J), Input)).
+findTangan(Nama, Input, Output) :- kartuPemain(Nama, kartu(W, J)), \+ isMember(kartu(W, J), Input), !, findTangan(Nama, [kartu(W, J)|Input], Output).
+findAllTangan(Nama, List) :- findTangan(Nama, [], List).
+
 /* Sumber Kode: https://stackoverflow.com/questions/72682057/implementing-a-simple-version-of-prolog-findall-without-using-the-built-in-finda */
