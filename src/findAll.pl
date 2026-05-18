3fa1f4b7e782c5f75b466a5d00b1d17c841f1419
ufaufaufa <faaa140307@gmail.com>
2026-05-18 22:51:52 +0700

    Add findAllKartuPemain to support lihatKartu predicate

--- src/findAll.pl
+++ src/findAll.pl
@@ -10,4 +10,8 @@ findNomorGiliran(Input, Input) :- \+ (nomorGiliran(X), isMember(X,Input)).
 findNomorGiliran(Input, Output) :- giliran(X), \+ isMember(X,Input), !, findNomorGiliran([X|Input], Output).
 findAllNomorGiliran(List) :- findNomorGiliran([], List).
 
+findKartuPemain(Nama, Input, Input) :- \+ (kartuPemain(Nama, K), \+ isMember(K, Input)).
+findKartuPemain(Nama, Input, Output) :- kartuPemain(Nama, K), \+ isMember(K, Input), !, findKartuPemain(Nama, [K|Input], Output).
+findAllKartuPemain(Nama, List) :- findKartuPemain(Nama, [], List).
+
 /* Sumber Kode: https://stackoverflow.com/questions/72682057/implementing-a-simple-version-of-prolog-findall-without-using-the-built-in-finda */
