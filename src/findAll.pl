fd627b3a337aaaecf31b49a106dbefb1bb5f919a
arinyeyy <arinikarimatun.ak@gmail.com>
2026-05-19 15:59:06 +0700

    refactor(player): remove redundant findAllTangan predicate

--- src/findAll.pl
+++ src/findAll.pl
@@ -10,8 +10,4 @@ findKartuPemain(Nama, Input, Input) :- \+ (kartuPemain(Nama, K), \+ isMember(K, 
 findKartuPemain(Nama, Input, Output) :- kartuPemain(Nama, K), \+ isMember(K, Input), !, findKartuPemain(Nama, [K|Input], Output).
 findAllKartuPemain(Nama, List) :- findKartuPemain(Nama, [], List).
 
-findTangan(Nama, Input, Input) :- \+ (kartuPemain(Nama, kartu(W, J)), \+ isMember(kartu(W, J), Input)).
-findTangan(Nama, Input, Output) :- kartuPemain(Nama, kartu(W, J)), \+ isMember(kartu(W, J), Input), !, findTangan(Nama, [kartu(W, J)|Input], Output).
-findAllTangan(Nama, List) :- findTangan(Nama, [], List).
-
 /* Sumber Kode: https://stackoverflow.com/questions/72682057/implementing-a-simple-version-of-prolog-findall-without-using-the-built-in-finda */
