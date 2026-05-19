b652bbcb68b3903090272e66944f9d3fa6a9333e
ajalku <thisneptunegirl@gmail.com>
2026-05-19 11:32:52 +0700

    feat: added uni, added aktifkanUniJika(Pemain) to main; fix: tangkap.pl

--- src/tangkap.pl
+++ src/tangkap.pl
@@ -2,10 +2,10 @@ tangkap(Pemain) :-
     kartuPemain(Pemain, Kartu),
     listLength(Kartu, N),
     (   N == 1 ->
-        (   \+ uniActivated(Pemain) ->
+        (   uniActivated(Pemain) ->
             write(Pemain), write(' tertangkap tidak menyerukan UNI.'), nl,
             write(Pemain), write(' mendapatkan 2 kartu pinalti'), nl,
-            ambil(Pemain, 2),!
+            ambil(Pemain, 2), retract(uniActivated(Pemain)), !
         ;   
             write('Perintah tidak valid! '), write(Pemain), write(' sudah menyerukan UNI.'), nl,
             giliran(PemainTantang), ambil(PemainTantang, 1),!
@@ -13,4 +13,4 @@ tangkap(Pemain) :-
     ;
         write('Perintah tidak valid! Kartu '), write(Pemain), write(' tidak berjumlah satu.'), nl,
         giliran(PemainTantang), ambil(PemainTantang, 1),!
-    ).
+    ),
