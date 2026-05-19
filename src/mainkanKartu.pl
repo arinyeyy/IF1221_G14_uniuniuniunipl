78e5255d5694c6070cf44a4175a44813b072e94f
swimmingparks <ivyychandra@gmail.com>
2026-05-19 08:07:11 +0700

    Update mainkanKartu.pl

--- src/mainkanKartu.pl
+++ src/mainkanKartu.pl
@@ -1,18 +1,45 @@
+:- include('kartuReverse.pl').
+:- include('kartuSkip.pl').
+
 mainkanKartu(Index) :-
     giliran(Pemain),
-    tangan(Pemain, ListKartu),
-    ambil_idx(Index, ListKartu, Kartu),
-    discard(KartuAtas),
+    findall(kartu(W,J),
+        kartuPemain(Pemain, kartu(W,J)),
+        DaftarKartu),
+
+    getElement(DaftarKartu, Index, kartu(Warna, Jenis)),
+    discardPileTop([kartu(WarnaTop, JenisTop)]),
+
     (
-        valid(Kartu, KartuAtas) ->
-        writeln(Pemain), write(' memainkan kartu: '), writeln(Kartu),
-        hapus_idx(Index, ListKartu, SisaKartu),
-        retract(tangan(Pemain, ListKartu)),
-        assertz(tangan(Pemain, SisaKartu)),
-        retract(discard(_)),
-        assertz(discard(Kartu)),
-        efek_kartu(Kartu)
-        ;
-        writeln('Kartu tidak valid!'),
-        fail
-    ).
+        Warna == WarnaTop ;
+        Jenis == JenisTop ;
+        Warna == hitam
+    ),
+
+    retract(kartuPemain(Pemain, kartu(Warna, Jenis))),
+    retractall(discardPileTop(_)),
+    asserta(discardPileTop([kartu(Warna, Jenis)])),
+
+    format('~w memainkan kartu ~w-~w.~n',
+        [Pemain, Warna, Jenis]),
+
+    efekKartu(kartu(Warna, Jenis)).
+
+efekKartu(kartu(_, reverse)) :-
+    kartuReverse, !.
+
+efekKartu(kartu(_, skip)) :-
+    kartuSkip, !.
+
+/* efekKartu(kartu(_, draw_two)) :-
+    skipKartu, !.
+
+efekKartu(kartu(_, wild)) :-
+    skipKartu, !. 
+    
+efekKartu(kartu(_, wild_draw_four)) :-
+    skipKartu, !. */
+
+efekKartu(_) :-
+    nomorGiliran(N),        % ambil N di sini
+    beriGiliranNormal(N).   % lalu pass
