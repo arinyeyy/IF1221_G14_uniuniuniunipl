fa9704d1cf23f24d12c43375eba4d6ac3d8e03e0
ufaufaufa <faaa140307@gmail.com>
2026-05-27 19:52:33 +0700

    refactor: Update and rename src/lihatCommand&lihatKartu.pl to lihatKartu.pl

--- lihatKartu.pl
+++ lihatKartu.pl
@@ -1,13 +1,3 @@
-/* lihatCommand :-
-    write('Aksi utama yang tersedia:'), nl,
-    write('1. ambilKartu'), nl,
-    write('2. tantang'), nl,
-    nl,
-    write('Aksi pendukung yang tersedia:'), nl,
-    write('1. lihatCommand'), nl,
-    write('2. lihatKartu'), nl,
-    write('3. cekInfo'), nl. */
-
 lihatKartu :-
     giliran(PemainAktif),
     write('Berikut kartu yang anda miliki.'), nl,
