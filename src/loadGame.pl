db6ca2babc2a6584eee319224e2f64c3c5085b08
ajalku <thisneptunegirl@gmail.com>
2026-05-31 02:55:47 +0700

    fix: loadGame can now be used correctly, all savegame

--- src/loadGame.pl
+++ src/loadGame.pl
@@ -4,19 +4,19 @@ daftarkanPemain([Nama|T]) :-
   daftarkanPemain(T).
 
 loadMode(S) :-
-  read(S, mode(Mode)),
+  read(S, mode:Mode),
   retractall(mode(_)),
   asserta(mode(Mode)).
 
 loadUrutanPemain(S) :-
-  read(S, urutan_pemain(Urutan)),
+  read(S, urutan_pemain:Urutan),
   retractall(allPemain(_)),
   retractall(pemain(_)),
   asserta(allPemain(Urutan)),
   daftarkanPemain(Urutan).
 
 loadGiliran(S) :-
-  read(S, giliran(PemainTerkini)),
+  read(S, giliran:PemainTerkini),
   retractall(giliran(_)),
   asserta(giliran(PemainTerkini)),
   allPemain(Urutan),
@@ -25,32 +25,36 @@ loadGiliran(S) :-
   asserta(nomorGiliran(Idx)).
 
 loadDiscardPileTop(S) :-
-  read(S, discard_top(W, J)),
+  read(S, discard_top:Term),
+  Term = W-J,
   retractall(discardPileTop(_)),
   asserta(discardPileTop([kartu(W, J)])).
 
 loadWarnaAktif(S) :-
-  read(S, warna_aktif(Warna)),
+  read(S, warna_aktif:Warna),
   retractall(warnaWildTerpilih(_)),
-  asserta(warnaWildTerpilih(Warna)).
+  asserta(warnaWildTerpilih(Warna)), !.
 loadWarnaAktif(_).
 
 loadArah(S) :-
-  read(S, arah_permainan(Arah)),
+  read(S, arah_permainan:Arah),
   retractall(arahMain(_)),
   asserta(arahMain(Arah)).
 
 loadStatusUNI(S) :-
-  read(S, status_UNI(StatUni)),
+  read(S, status_uni:StatUni),
   retractall(riwayatUNI(_)),
   asserta(riwayatUNI(StatUni)).
 
-loadKartuPemain(S) :- allPemain(Urutan), listLength(Urutan, N), helperLoadKartuPemain(S, N).
+loadKartuPemain(S) :-
+  allPemain(Urutan),
+  listLength(Urutan, N),
+  helperLoadKartuPemain(S, N).
 
 helperLoadKartuPemain(_, 0) :- !.
 helperLoadKartuPemain(S, N) :-
   read(S, Line),
-  Line = kartu(Pemain, ListKartu),
+  Line = kartu(Pemain):ListKartu,
   retractall(kartuPemain(Pemain,_)),
   daftarkanKartuSatuPemain(Pemain, ListKartu),
   N1 is N-1,
@@ -58,12 +62,45 @@ helperLoadKartuPemain(S, N) :-
 
 daftarkanKartuSatuPemain(_, []) :- !.
 daftarkanKartuSatuPemain(Pemain, [Kartu|Sisa]) :-
-  asserta(kartuPemain(Pemain, Kartu)),
+  Kartu = W-J,
+  asserta(kartuPemain(Pemain, kartu(W, J))),
   daftarkanKartuSatuPemain(Pemain, Sisa).
 
+loadKartuAksiTerakhir(S) :-
+  read(S, kartu_aksi_terakhir:Term),
+  (
+    Term == tidak_ada ->
+    retractall(kartuAksiTerakhir(_,_)),
+    retractall(giliranAksiTerakhir(_,_))
+  ;
+    Term = ((W-J)-Pemain)-Count,
+    retractall(kartuAksiTerakhir(_,_)),
+    asserta(kartuAksiTerakhir(W, J)),
+    retractall(giliranAksiTerakhir(_,_)),
+    asserta(giliranAksiTerakhir(Pemain,Count))
+  ).
+
+loadKartuTersembunyi(S) :-
+  allPemain(Urutan),
+  listLength(Urutan, N),
+  helperLoadKartuTersembunyi(S, N).
+
+helperLoadKartuTersembunyi(_, 0) :- !.
+helperLoadKartuTersembunyi(S, N) :-
+  read(S, kartu_tersembunyi(Pemain):ListKartuTersembunyi),
+  retractall(kartuTersembunyi(Pemain, _)),
+  daftarkan1PemainKT(Pemain, ListKartuTersembunyi),
+  N1 is N - 1,
+  helperLoadKartuTersembunyi(S, N1).
+
+daftarkan1PemainKT(_, []) :- !.
+daftarkan1PemainKT(Pemain, [H|T]) :-
+  asserta(kartuTersembunyi(Pemain, H)),
+  daftarkan1PemainKT(Pemain, T).
+
 loadTim(S) :-
-  read(S, tim1(Tim1)),
-  read(S, tim2(Tim2)),
+  read(S, tim1:Tim1),
+  read(S, tim2:Tim2),
   retractall(tim(_, _)),
   retractall(setim(_, _)),
   asserta(tim(1, Tim1)),
@@ -79,6 +116,7 @@ loadGame :-
   write('Masukkan nama file yang akan dimuat: '), read(NamaFile), nl,
   sambung_txt(NamaFile, FileNama),
   (   open(FileNama, read, S) ->
+      asserta(gameStarted),
       loadMode(S),
       loadUrutanPemain(S),
       loadGiliran(S),
@@ -87,6 +125,8 @@ loadGame :-
       loadArah(S),
       loadStatusUNI(S),
       loadKartuPemain(S),
+      loadKartuAksiTerakhir(S),
+      loadKartuTersembunyi(S),
       (mode(2) -> loadTim(S) ; true),
       close(S),
       format('Status permainan berhasil dimuat dari ~w.~n', [FileNama]),
